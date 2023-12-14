93f11436bbc0844eb404ad7c9b121aba99b7ba91
Bacem Dallel <111270554+BacemDallel@users.noreply.github.com>
2023-12-14 12:57:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ db = SQLAlchemy()
 db.init_app(app)
 
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?"
-MOVIE_DB_API_KEY = '1e72f334eff54926c31ee61ed34ebd96'
+MOVIE_DB_API_KEY = 'YOUR API KEY'
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = 'https://image.tmdb.org/t/p/original/'
 
